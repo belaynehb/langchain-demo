98177fb260cdec8aabac8907aaa4c18f2f948eaf
Belayneh Beje <belaynehworku@gmail.com>
2025-11-17 22:18:40 -0500

    second attempt after adding vector_stores files

--- 07.chat_history/07.4-chat_history_demo.py
+++ 07.chat_history/07.4-chat_history_demo.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 
-#from langchain_community.chat_models import ChatOllama
+# from langchain_community.chat_models import ChatOllama
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_community.chat_message_histories.in_memory import ChatMessageHistory
 from langchain_core.runnables import RunnableWithMessageHistory
@@ -11,13 +11,16 @@ from langchain_core.runnables import RunnableWithMessageHistory
 load_dotenv()
 
 api_key = os.getenv("OPENAI_API_KEY")
-llm=ChatOpenAI(model="gpt-4o", api_key=api_key)
-#llm=ChatOllama(model="mistral:latest")
+llm = ChatOpenAI(model="gpt-4o", api_key=api_key)
+# llm=ChatOllama(model="mistral:latest")
 prompt_template = ChatPromptTemplate(
     [
-        ("system", "You are Agile Coach. Answer any questions related to the agile process"),
+        (
+            "system",
+            "You are Agile Coach. Answer any questions related to the agile process",
+        ),
         MessagesPlaceholder(variable_name="chat_history"),
-        ("human" , "{input}")
+        ("human", "{input}"),
     ]
 )
 
@@ -27,15 +30,17 @@ history_for_chain = ChatMessageHistory()
 
 chain_with_history = RunnableWithMessageHistory(
     chain,
-    lambda session_id:history_for_chain,
+    lambda session_id: history_for_chain,
     input_messages_key="input",
-    history_messages_key="chat_history"
+    history_messages_key="chat_history",
 )
 
 print("Agile Guide")
 
 while True:
-    question =input("Enter your agile question here:")
+    question = input("Enter your agile question here:")
     if question:
-        response = chain_with_history.invoke({"input":question}, config={"configurable":{"session_id":"abc123"}})
+        response = chain_with_history.invoke(
+            {"input": question}, config={"configurable": {"session_id": "abc123"}}
+        )
         print(response.content)

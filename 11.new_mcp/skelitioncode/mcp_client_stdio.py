5e8d10c686d3e5ed5c79e7db748984051f0e5938
Belayneh Beje <belaynehworku@gmail.com>
2025-11-20 20:39:51 -0500

    add folder 11.0-new_mcp

---
+++ 11.new_mcp/skelitioncode/mcp_client_stdio.py
@@ -0,0 +1,35 @@
+import asyncio
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+from langchain_openai import ChatOpenAI
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langchain.agents import create_agent
+
+load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+
+client = MultiServerMCPClient(
+    {
+        "tools": {
+            "command": "python",
+            "args": ["mcp_server_stdio.py"],
+            "transport": "stdio",
+        }
+    }
+)
+tools = asyncio.run(client.get_tools())
+
+llm = ChatOpenAI(model="gpt-4o")
+agent = create_agent(llm, tools)
+
+st.title("AI Agent (MCP Version)")
+task = st.text_input("Assign me a task")
+
+if task:
+    response = asyncio.run(agent.ainvoke({"messages": task}))
+    final_output = response["messages"][-1].content
+    st.write(final_output)

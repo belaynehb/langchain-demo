7a6afc279d2f02da4c1857e480c0f144941b1912
Belayneh Beje <belaynehworku@gmail.com>
2025-11-22 19:18:39 -0500

    second round gemma_demo and mistral_demo

--- 04.basics/04.3-mistral_demo.py
+++ 04.basics/04.3-mistral_demo.py
@@ -1,15 +1,31 @@
-#import os
-#from dotenv import load_dotenv
+# #import os
+# #from dotenv import load_dotenv
 
-#from langchain_openai import ChatOpenAI
+# #from langchain_openai import ChatOpenAI
+# from langchain_community.chat_models import ChatOllama
+
+# #load_dotenv()
+
+# #api_key = os.getenv("OPENAI_API_KEY")
+# #llm=ChatOpenAI(model="gpt-4o", api_key=api_key)
+# llm=ChatOllama(model="mistral:latest")
+
+# question=input("Enter the question: \n")
+# response = llm.invoke(question)
+# print(response.content)
+
 from langchain_community.chat_models import ChatOllama
 
-#load_dotenv()
+llm = ChatOllama(model="mistral")
 
-#api_key = os.getenv("OPENAI_API_KEY")
-#llm=ChatOpenAI(model="gpt-4o", api_key=api_key)
-llm=ChatOllama(model="mistral:latest")
-
-question=input("Enter the question: \n")
+question = input("Write your question \n")
 response = llm.invoke(question)
 print(response.content)
+
+# from langchain_mistralai.chat_models import ChatMistralAI
+
+# llm = ChatMistralAI(model="mistral")
+
+# question = input("Write your question here \n")
+# response = llm.invoke(question)
+# print(response.content)

4c975564b8b6eabb38ef064702820e7607ed43fd
Belayneh Beje <belaynehworku@gmail.com>
2025-12-01 23:06:27 -0500

    add google api code in 04.1 file and tested successful

--- 04.basics/04.1-openai_demo.py
+++ 04.basics/04.1-openai_demo.py
@@ -12,30 +12,42 @@
 # response = llm.invoke(question)
 # print(response.content)
 
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-from langchain_openai import ChatOpenAI
-
-api_key = os.getenv("OPENAI_API_KEY")
-llm = ChatOpenAI(model="gpt-4o", api_key=api_key)
-
-question = input("Write your question here \n")
-response = llm.invoke(question)
-print(response.content)
-
-
 # import os
-
 # from dotenv import load_dotenv
-# from langchain_openai import ChatOpenAI
 
 # load_dotenv()
+# from langchain_openai import ChatOpenAI
 
-# API_KEY = os.getenv("GOOGLE_API_KEY")
-# llm = ChatOpenAI(model="gemini", api_key=API_KEY)
+# api_key = os.getenv("OPENAI_API_KEY")
+# llm = ChatOpenAI(model="gpt-4o", api_key=api_key)
 
-# question = input("Enter the question: \n")
+# question = input("Write your question here \n")
 # response = llm.invoke(question)
 # print(response.content)
+
+## USING GOOGLE GEMINI
+import os
+import getpass
+
+from dotenv import load_dotenv
+
+# from langchain_google_vertexai import ChatVertexAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
+if "GOOGLE_API_KEY" not in os.environ:
+    os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter your Google AI API key: ")
+
+# API_KEY = os.getenv("GOOGLE_API_KEY")
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+)
+
+question = input("Enter the question: \n")
+response = llm.invoke(question)
+print(response.content)
--- 04.basics/04.2-gemma_demo.py
+++ 04.basics/04.2-gemma_demo.py
@@ -1,12 +1,12 @@
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
 
 # from langchain_openai import ChatOpenAI
 from langchain_community.chat_models import ChatOllama
 
-load_dotenv()
+# load_dotenv()
 
-api_key = os.getenv("OPENAI_API_KEY")
+# api_key = os.getenv("OPENAI_API_KEY")
 # llm=ChatOpenAI(model="gpt-4o", api_key=api_key)
 llm = ChatOllama(model="gemma:2b")
 
